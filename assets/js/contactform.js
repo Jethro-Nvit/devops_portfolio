97a0efe3ab4605160b9a6df0143cb6b8fb502f34
Jethro-Nvit <160114839+Jethro-Nvit@users.noreply.github.com>
2024-11-04 19:08:29 +0100

    Update contactform.js

--- assets/js/contactform.js
+++ assets/js/contactform.js
@@ -1,1 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzOexuD00tdh0t5C-6NecypkAjfb2tli1lelK6cKCDs5ApEYw0LhgAMXxfHESbb9cNwSQ/exec'
 
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+ e.preventDefault()
+ fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+ .then(response => alert("Thank you! your form is submitted successfully." ))
+ .then(() => { window.location.reload(); })
+ .catch(error => console.error('Error!', error.message))
+})
